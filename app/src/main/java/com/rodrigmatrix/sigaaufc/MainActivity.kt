19b3b56b88c8fe2d39f19d9e2224b19b3afdfde5
rodrigmatrix <rodrigmatrix@gmail.com>
2019-07-02 01:32:53 -0300

    Color nav drawer

--- app/src/main/java/com/rodrigmatrix/sigaaufc/MainActivity.kt
+++ app/src/main/java/com/rodrigmatrix/sigaaufc/MainActivity.kt
@@ -24,12 +24,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_sigaa, R.id.nav_ru, R.id.nav_library,
@@ -62,7 +59,7 @@ class MainActivity : AppCompatActivity() {
 //                }
 //            }
         }
-        setDefaultNightMode(MODE_NIGHT_NO)
+        //setDefaultNightMode(MODE_NIGHT_NO)
         //setDefaultNightMode(MODE_NIGHT_YES)
     }
 
